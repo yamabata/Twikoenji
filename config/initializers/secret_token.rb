66a5421548cfe088e608b83d864d13d5b1d0a69a
hiromitu <hiromitsu-itou@ngigroup.com>
2012-04-28 14:48:38 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twikoenji::Application.config.secret_token = '86548ed9e17378c6204083965433320ca7d2713bd8ddb7b851351e0864d253a96ac3a4dbd94feb0a4203f0e9fcb6a5981b57b1f0a7ce8766b73a37c9464f69f6'
